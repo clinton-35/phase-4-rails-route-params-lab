39e615446d68654cd9550e3ee43b57db435136f2
clinton <clinton.wambua@student.moringaschool.com>
2023-06-20 11:53:34 +0300

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,19 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  def index
+    if params[:query].present?
+      students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      students = Student.all
+    end
+    render json: students
+  end
+  
+  
 
 end
